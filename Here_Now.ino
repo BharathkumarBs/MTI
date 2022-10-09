0aa382f2cddc6e5692e20bb20dc8ebd0acdaf06a
BharathkumarBs <70940379+BharathkumarBs@users.noreply.github.com>
2022-10-10 08:57:29 +1100

    Add files via upload

--- Here_Now.ino
+++ Here_Now.ino
@@ -1,9 +1,7 @@
-
-//Constants
 const int ledPin = 5;   //pin 3 has PWM funtion
 const int flexPin = A0; //pin A0 to read analog input
 const int motorPin = 4; //pin 4 has output pin
-const int realyPin = 3; //pin 3 has output pin 
+/* const int realyPin = 3; //pin 3 has output pin */
 
 //Variables:
 int flexValue; //save analog flexvalue
@@ -12,8 +10,8 @@ int flexValue; //save analog flexvalue
 void setup(){
   
   pinMode(ledPin, OUTPUT); //Set pin 5 as 'output'
-  pinMode(motorPin. OUTPUT); //Set pin 4 as 'output'
-  pinMode(relayPin. OUTPUT); //Set pin 4 as 'output'
+  pinMode(motorPin, OUTPUT); //Set pin 4 as 'output'
+  /*pinMode(relayPin, OUTPUT); //Set pin 4 as 'output'*/
   Serial.begin(9600); //Begin serial communication
 
 }
@@ -21,17 +19,17 @@ void setup(){
 void loop(){
   
   flexValue = analogRead(flexPin);         //Read and save analog value 
-  value = map(value, 700, 900, 0, 255);//Map value 0-1023 to 0-255 (PWM)
-  If(flexValue > 0 )
+  flexValue = map(flexValue, 700, 900, 0, 255);//Map value 0-1023 to 0-255 (PWM)
+  if(flexValue > 255 )
     {
-      digitalWrite(motorPin, High);
-      digitalWrite(relayPin, High);
-      analogWrite(ledPin, value);  
+      digitalWrite(motorPin, HIGH);
+      /*digitalWrite(relayPin, High);*/
+      analogWrite(ledPin, flexValue);  
       delay(1000);
-      digitalWrite(motorPin, Low);
-      digitalWrite(relayPin, Low);
+      digitalWrite(motorPin, LOW);
+      /*digitalWrite(relayPin, Low);*/
     }
     
-  Serial.println(value);               //Print value
+  Serial.println(flexValue);               //Print value
   
 }
